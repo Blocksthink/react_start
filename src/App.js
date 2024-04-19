34505582aa30ea20547b5bbdc67a611f1805146b
System Administrator <root@Graceui-MacBookAir.local>
2024-04-19 23:18:59 +0200

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
+/*eslint-disable*/
+
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let [글제목, 글제목변경] = useState(['남자 코트 추천','강남우동맛집','파이썬독학']);
+  let [따봉, 따봉변경 ] = useState(0);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 blog</div>
+        </div>
+        <div className="list">
+          <h1>{ 글제목[0] } <span onClick={()=>{ 따봉변경(따봉+1)}}> 😍 </span> { 따봉 } </h1>
+          <p>2/17 발행 
+          <button onClick={()=> {
+            let copy = [...글제목];
+            copy.sort();
+            글제목변경(copy)
+            }}>정렬</button>
+          </p>
+        </div> 
+        <div className="list">
+          <h1>{ 글제목[1] }</h1>
+          <p>2/17 발행</p>
+        </div>
+        <div className="list">
+          <h1>{ 글제목[2] }</h1>
+          <p>2/17 발행</p>
+        </div>
     </div>
   );
 }
